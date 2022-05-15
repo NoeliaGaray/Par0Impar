7263be59af1581fabf619f97f3f87fc44dbdb760
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-15 18:45:29 -0300

    solucionado lo del 0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 for (let i = 1; i <= 1; i++) {
   i = prompt("Ingrese un número para saber si es par o impar");
-  if (i % 2 === 0) {
-    console.log("El número es par");
-  } else if (i % 2 === 1) {
-    console.log("El número es impar");
-  } else if (i === 0) {
+  if (i == 0) {
     console.log("Ingrese un número diferente a 0");
+    break;
+  } else if (i % 2 == 1) {
+    console.log("El número es impar");
+    break;
+  } else if (i % 2 == 0) {
+    console.log("El número es par");
+    break;
   }
 }
